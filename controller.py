57db21532f687a4a3b928b1347c2e434971fddb8
matejt <matejrpls@gmail.com>
2018-03-18 13:11:59 -0500

    VRP object methods developed further

--- controller.py
+++ controller.py
@@ -8,15 +8,21 @@ logging.basicConfig(level=logging.INFO)
 class VRP(object):
 	def __init__(self, url):
 		self.url = url
+		self.routes = get_routes()
+		self.depots = get_depots()
+		
 
 	def solve(self, orders):
 		if orders:
 			# do something
 			pass
+			jobid = self.submit(orders)
+			return jobid
 
-	def check_status(self):
-		# how do we want to check status?
-		pass
+	def check_status(self, jobid):
+		if jobid:
+			# how do we want to check status?
+			pass
 
 	def pull_results(self, jobid):
 		# how do we want to pull results
