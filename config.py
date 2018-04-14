483ec5ccab6133c25aff3640e237e13f7ed44296
matejt <matejrpls@gmail.com>
2018-04-14 13:10:47 -0500

    config concept improved

--- config.py
+++ config.py
@@ -1,7 +1,21 @@
 # logic to detect the environment
 
+def detect_env():
+	if something.exist:
+		# extract the env symbol
+		env = 'w'
 
-conf = {}  #some unuseful comment
+	if env in ('w', 'd', 's', 'c', 'p'):
+		return env
+	else raise Exception('Unknown environment: {}'.format(env)
 
 
+envs = {
+	'w': {},
+	'd': {},
+	's': {},
+	'c': {},
+	'p': {},
+}
 
+conf = envs.get(detect_env())
